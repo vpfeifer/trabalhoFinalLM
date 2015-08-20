1963fcbff312678d58cad3fa46f315785ee4e6a4
Marco Antonio Bordon <marcoabordon@gmail.com>
2015-08-20 20:10:11 -0300

    Comentano prints de debug

--- main/calc_c.c
+++ main/calc_c.c
@@ -18,10 +18,10 @@
 
 int calc_c(int tamanho, int escalar, int * matrizA, int * matrizB)
 {
-  printf("\nMatriz A");
-  PrintMatriz(tamanho, matrizA);
-  printf("\nMatriz B");
-  PrintMatriz(tamanho, matrizB);
+  // printf("\nMatriz A");
+  // PrintMatriz(tamanho, matrizA);
+  // printf("\nMatriz B");
+  // PrintMatriz(tamanho, matrizB);
 
   int i = 0;
   int j = 0;
@@ -39,7 +39,7 @@ int calc_c(int tamanho, int escalar, int * matrizA, int * matrizB)
       auxSoma += escalar * matrizA[i * tamanho + k] * matrizB[k * tamanho + i];
     }
 
-    printf("%d ", auxSoma);
+    // printf("%d ", auxSoma);
 
     if(auxSoma < menorValor)
     {
